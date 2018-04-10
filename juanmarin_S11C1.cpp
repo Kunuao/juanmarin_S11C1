74d2ee773c0979110db2c1abb510131d1e1744a2
Kunuao <37352285+Kunuao@users.noreply.github.com>
2018-04-10 18:27:23 -0500

    Add files via upload

---
+++ juanmarin_S11C1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <ctime>
+using namespace std; 
+#define h=0.01
+                
+
+
+double S1(double betha, double h, I);
+
+double I(double betha, double gamma, double S, I, double h);
+
+
+double S(double betha, double h, double S, I){
+	double resp = 
+
+}
+
+
+
+int main() {
+
+int N = 771;
+double gamma1 = 0.45 
+double betha1 = 0.0022;
+double betha2 = 0.001; 
+double gamma2 = 0.2;
+
+
+
+
+
+
+
+
+return 0 ; 
+} 
+
+
